398716441d6273f377b351ed29a6b3ae6733751b
Luiz Pansarini <lpansarini@gmail.com>
2021-08-28 10:14:26 -0300

    Chapter 1 - Configurando Ambiente

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },

fa2ced205fad0fb2adc10a3346f32b48ecec8917
Luiz Pansarini <lpansarini@gmail.com>
2021-08-29 16:16:04 -0300

    Chapter 1 - Chamadas HTTP

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
-      <a href={props.repository.link}>Acessar o repositório</a>
+      <a href={props.repository.html_url}>Acessar o repositório</a>
     </li>
   );
 }
